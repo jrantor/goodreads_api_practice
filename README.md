9699f1155601199905e3d3e4aedc376ed3b3ea85
Antor <crewshedg@gmail.com>
2020-11-04 22:15:45 +0600

    Added try..catch and usage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### This was fun to work with the Goodreads API.
+### This is just a simple wrap of Goodreads API.
 
-*This class does not use cahing responses.*
+*This class does not use caching responses.*
 
 ### Available Methods
 
@@ -8,6 +8,21 @@
 - [search.books](https://www.goodreads.com/api/index#search.books)
 - [book.title](https://www.goodreads.com/api/index#book.title)
 
+
+***Usage***
+
+```
+// initializing the api
+  $api = new Goodreads(YOUR_API_KEY);
+
+// getting response in an array(key,value)
+  $api_response = $api->get_books_by_author(AUTHOR_ID);
+
+// print with print_r
+
+ print_r($api_response);
+
+```
 
 Inspired by [Goodreads-api](https://github.com/danielgwood/goodreads-api)
 
--- src/Goodreads.php
+++ src/Goodreads.php
@@ -2,13 +2,27 @@
 
 class Goodreads{
 
+    // main url for api
+
 	const ROOT_URL = "https://www.goodreads.com";
+
+	// initializing apikey var
 
 	protected $api_key = '';
 
+	// getting apikey during object creation
 	public function __construct($api_key){
 		$this->api_key = (string)$api_key;
 	}
+
+	/*
+	 * Searching books with title, isbn, or author
+	 * 
+	 * @param string $query Search keyword
+	 * 
+	 * @return array
+	 *
+	 */
 
 	public function search_with_query($query){
 		return $this->request('search',array(
@@ -16,6 +30,17 @@ class Goodreads{
 			'q' => $query
 		));
 	}
+
+	/*
+	 * Get reviews of a given title & author name
+	 *
+	 * @param string $title Title of the book
+	 *
+	 * @param string $author Author's name of the book
+	 *
+	 * @return array
+	 *
+	 */
 
 	public function get_review($title,$author){
 		return $this->request(
@@ -27,16 +52,40 @@ class Goodreads{
 		);
 	}
 
+	/*
+	 * Get books by an author given author id
+	 * 
+	 * @param integer $id Author's ID
+	 * 
+	 * @return array
+	 *
+	 */
+
 	public function get_books_by_author($id){
 		return $this->request('author/list', array(
 			'key' => $this->api_key,
-			'id' => $id,
+			'id' => (int)$id,
 		));
 	}
 
+	/*
+	 * Sending request to specified endpoint and getting response
+	 *
+	 * @param string $endpoint API endpoint
+	 *
+	 * @param array $params API endpoint parameters
+	 *
+	 * @throws exception
+	 *
+	 * @return array
+	 *
+	 */
 
 	private function request($endpoint,$params){
+		// setting url
 		$url = self::ROOT_URL.'/'.$endpoint.'?'.((!empty($params)) ? http_build_query($params,'','&') : '');
+		
+		// headers
 		$headers = array(
 			'Accept: application/xml'
 		);
@@ -48,38 +97,38 @@ class Goodreads{
 			);
 		}
 
-		if(extension_loaded('curl')){
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-		
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_HEADER, 1);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$response = curl_exec($ch);
-			$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-			$header = substr($response, 0,$header_size);
-			$result = substr($response, $header_size);
-			$error_no = curl_errno($ch);
-			$error_message = curl_error($ch);
+		// curl extension
+        try {
+                $ch = curl_init(); // initializing curl
 
-			if($error_no > 0) throw new Exception("Error Processing Request on ".$endpoint." ".$error_message);	
+                curl_setopt($ch, CURLOPT_URL, $url); //url set
 
-			 curl_close($ch);	
-		} else{
-			throw new Exception("Error Processing curl library.");
-			
-		}
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // setting headers
+                curl_setopt($ch, CURLOPT_HEADER, 1); // with header true
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Request return true
+                $response = curl_exec($ch);
+                $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+                $header = substr($response, 0,$header_size);
+                $result = substr($response, $header_size);
+                curl_close($ch);	// closing curl
 
-		if(isset($params['format']) && $params['format'] === 'json'){
-			$result = json_decode($result);
-		}
 
-		else{
-			$result = json_decode(json_encode((array)simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA)),1);
-		}
+        }
 
-		if(!empty($result)) return $result;
-		else throw new Exception("Error Getting Results");
+		catch (Throwable $exception){
+		    echo "Error Processing Request on ".$endpoint." ".$exception->getMessage();
+        }
+
+        if(isset($params['format']) && $params['format'] === 'json'){
+            $result = json_decode($result); // json decoding
+        }
+
+        else{
+            // getting xml data to json and then to an array
+            $result = json_decode(json_encode((array)simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA)),1);
+        }
+
+        if(!empty($result)) return $result;
 	
 	}
 }
